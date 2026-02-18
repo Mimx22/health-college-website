2b0e54341cf1fa11288fd78ff55fd83ecb975273
mimx22 <glittercost@gmail.com>
2026-02-18 07:34:55 +0100

    Add admin GET /api/students route using getAllStudents

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerStudent, loginStudent, updateProfile, approveStudent } = require('../controllers/studentController');
+const { registerStudent, loginStudent, updateProfile, approveStudent, getAllStudents } = require('../controllers/studentController');
 const { adminAuth } = require('../middleware/authMiddleware');
 
 router.post('/register', registerStudent);
 router.post('/login', loginStudent);
+router.get('/', adminAuth, getAllStudents);
 router.put('/profile', updateProfile);
 router.patch('/:id/approve', adminAuth, approveStudent);
 
