256ad152f6409329fc03914169d4b39ca27b8afe
mimx22 <glittercost@gmail.com>
2026-02-20 20:30:41 +0100

    cleanup: remove legacy backend files after repo separation

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { forgotPassword, resetPassword, loginUser } = require('../controllers/authController');
-
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password/:token', resetPassword);
-router.post('/login', loginUser);
-
-module.exports = router;
