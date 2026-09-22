22ccb550d73331c2ec29ead5b816030e0e202755
unknown <antigravity@example.com>
2026-09-22 14:54:26 +0100

    Fix password case: %24Miracle2255 (capital M)

--- api/config/db.js
+++ api/config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
         }
 
         // Fallback to the raw connection string if environment variable is overridden by cPanel
-        const rawUri = 'mongodb://glittercost_db_user:%24miracle2255@ac-nigzd1w-shard-00-00.lv0fc0l.mongodb.net:27017,ac-nigzd1w-shard-00-01.lv0fc0l.mongodb.net:27017,ac-nigzd1w-shard-00-02.lv0fc0l.mongodb.net:27017/josmedicalcollege?ssl=true&replicaSet=atlas-123aol-shard-0&authSource=admin&retryWrites=true&w=majority';
+        const rawUri = 'mongodb://glittercost_db_user:%24Miracle2255@ac-nigzd1w-shard-00-00.lv0fc0l.mongodb.net:27017,ac-nigzd1w-shard-00-01.lv0fc0l.mongodb.net:27017,ac-nigzd1w-shard-00-02.lv0fc0l.mongodb.net:27017/josmedicalcollege?ssl=true&replicaSet=atlas-123aol-shard-0&authSource=admin&retryWrites=true&w=majority';
         const conn = await mongoose.connect(rawUri, { serverSelectionTimeoutMS: 60000 });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
         
